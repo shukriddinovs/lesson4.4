31b9ccf89d28af5cccb0e799cf15ba1e79a8e7ef
shukriddinovs <sshukriddinov524@gmail.com>
2024-04-23 14:26:25 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,19 @@
+let random = Math.floor(Math.random() * 100);
+
+console.log(random);
+
+if (random % 3 === 0 && random % 5 === 0) {
+    console.log(random + "bu son FizzBazz");
+}else if (random % 3 === 0){
+    console.log(random + "bu son Fizz");
+    
+
+}else if (random % 5 === 0){
+    console.log(random + "bu son Bazz");
+
+}
+if(random % 3 ===1,4,6,7,8,9. && random % 5 === 1,2,4,6,7,8,9.  ){
+    console.log(random = "bu FizzBazz ham emas");
+}else if(random % 3 ===1 && random % 5 === 1){
+    console.log(random = "bu FizzBazz ham emas");
+}
